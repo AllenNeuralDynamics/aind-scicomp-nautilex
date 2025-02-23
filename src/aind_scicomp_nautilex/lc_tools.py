5988297fece9592e68c3b805aba95f6b5038fe16
Dan Birman <danbirman@gmail.com>
2025-02-23 12:52:09 -0800

    fix: stupid mistake pointing to dev docdb

--- src/aind_scicomp_nautilex/lc_tools.py
+++ src/aind_scicomp_nautilex/lc_tools.py
@@ -3,7 +3,7 @@ from aind_data_access_api.document_db import MetadataDbClient
 from typing import List
 import os
 
-API_GATEWAY_HOST = os.getenv("API_GATEWAY_HOST", "api.allenneuraldynamics-test.org")
+API_GATEWAY_HOST = os.getenv("API_GATEWAY_HOST", "api.allenneuraldynamics.org")
 DATABASE = os.getenv("DATABASE", "metadata_index")
 COLLECTION = os.getenv("COLLECTION", "data_assets")
 
